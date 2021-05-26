cbaa5a3cc2e2833216656ee08d9fe368e1bc36b6
sandeep <ankurmall89@gmail.com>
2021-05-26 22:34:33 +0530

    hash based approach

--- array/src/com/sks/array/easy/TwoSum.java
+++ array/src/com/sks/array/easy/TwoSum.java
@@ -1,6 +1,8 @@
 package com.sks.array.easy;
 
 
+import java.util.Map;
+
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  * <p>
@@ -31,5 +33,17 @@ public class TwoSum {
         }
         throw new IllegalArgumentException("No two sum solution");
 
+    }
+
+    public int[] twoSumHashing(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
