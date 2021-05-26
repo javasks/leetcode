b88d2ef9ef4d103206bc7e5101e3355f375290f7
sandeep <ankurmall89@gmail.com>
2021-05-26 22:16:50 +0530

    Brute force approach

--- array/src/com/sks/array/easy/TwoSum.java
+++ array/src/com/sks/array/easy/TwoSum.java
@@ -1,4 +1,35 @@
 package com.sks.array.easy;
 
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * You can return the answer in any order.
+ * <p>
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ */
 public class TwoSum {
+
+    /**
+     * Brute Force Approach
+     * @param nums
+     * @param target
+     * @return
+     */
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+
+    }
 }
